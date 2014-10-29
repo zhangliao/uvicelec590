479d19a20a73ff7bcf368a06cd3067e1203878fa
UNP1 <liaoz@ELEC590.com>
2014-10-28 22:46:10 -0700

    New POC codes with SSL feature.

--- poc/client.c
+++ poc/client.c
@@ -13,10 +13,30 @@
 #include<fcntl.h>  
   
 #include<errno.h>  
-  
+#include <openssl/ssl.h>
+#include <openssl/err.h>  
 #define oops(msg) { perror(msg); exit(errno); }  
+void ShowCerts(SSL * ssl)
+{
+	X509 *cert;
+	char *line;
+	cert = SSL_get_peer_certificate(ssl);
+	if (cert != NULL) {
+		printf("Digital Certification Information:\n");
+		line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
+		printf("Certification: %s\n", line);
+		free(line);
+		line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
+		printf("Issuer: %s\n", line);
+		free(line);
+		X509_free(cert);
+	} 
+	else
+		printf("无证书信息！\n");
+}
   
 int main(int argc, char *argv[]){  
+		
         if(argc != 2)oops("which file not specified?");  
   
         int sd = socket(PF_INET, SOCK_STREAM, 0);  
@@ -27,25 +47,68 @@ int main(int argc, char *argv[]){
         address.sin_family = AF_INET;  
         address.sin_addr.s_addr = inet_addr("192.168.0.1");  
         address.sin_port =  8888;  
-  
+		
+		SSL_CTX *ctx;
+		SSL *ssl;
+		
+		/* SSL 库初始化，参看 ssl-server.c 代码 */
+		SSL_library_init();
+		OpenSSL_add_all_algorithms();
+		SSL_load_error_strings();
+		ctx = SSL_CTX_new(SSLv23_client_method());
+		if (ctx == NULL) {
+			ERR_print_errors_fp(stdout);
+			exit(1);
+		}
+		
+		
         int connect_ret = connect(sd, (struct sockaddr*)&address, sizeof(address));  
         if(connect_ret == -1) oops("connect");  
-  
-        int fd = open(argv[1], O_RDONLY);  
-        off_t offset = 0;  
-        struct stat fileStatus;  
-        int fstat_ret = fstat(fd, &fileStatus);  
-        if(fstat_ret == -1)oops("fstat");  
+		
+		/* 基于 ctx 产生一个新的 SSL */
+		ssl = SSL_new(ctx);
+		SSL_set_fd(ssl, sd);
+		
+		/* 建立 SSL 连接 */
+		if (SSL_connect(ssl) == -1)
+			ERR_print_errors_fp(stderr);
+		else {
+			printf("Connected with %s encryption\n", SSL_get_cipher(ssl));
+			ShowCerts(ssl);
+		}
+		
+		char buffer[1024]; 
+        //int fd = open(argv[1], O_RDONLY);  
+		FILE *fd = fopen(argv[1], "r"); 
+        //off_t offset = 0;  
+        //struct stat fileStatus;  
+        //int fstat_ret = fstat(fd, &fileStatus);  
+        //if(fstat_ret == -1)oops("fstat");  
 		//char fileName[50]; 
 		printf("The filename is: %s\n",argv[1]);
-		send(sd,argv[1],strlen(argv[1]),0);
-        ssize_t length = sendfile(sd, fd, &offset, fileStatus.st_size);  
-        if(length == -1)  
+		SSL_write(ssl,argv[1],strlen(argv[1]));
+		int file_block_length = 0; 
+		while( (file_block_length = fread(buffer, sizeof(char), 1024, fd)) > 0)  
+            {  
+                printf("file_block_length = %d\n", file_block_length);  
+  
+                // 发送buffer中的字符串到new_server_socket,实际上就是发送给客户端  
+                if (SSL_write(ssl, buffer, file_block_length) < 0)  
+                {  
+                    printf("Send File:\t%s Failed!\n", argv[1]);  
+                    break;  
+                }  
+  
+                bzero(buffer, sizeof(buffer));  
+            }  
+		
+        //ssize_t length = sendfile(sd, fd, &offset, fileStatus.st_size);  
+        if(file_block_length == -1)  
                 {oops("sendfile");}  
         else  
-                fprintf(stdout, "%ld characters have been send to server.\n", length);  
+                //fprintf(stdout, "%ld characters have been send to server.\n", file_block_length);  
   
-        close(fd);  
+        fclose(fd);  
   
         close(sd);  
         return EXIT_SUCCESS;  
--- poc/server.c
+++ poc/server.c
@@ -13,9 +13,45 @@
 #include<time.h>  
   
 #include<errno.h>  
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 #define oops(msg) { perror(msg); exit(errno); }  
   
 int main(){  
+		
+		SSL_CTX *ctx;
+
+		SSL_library_init();/* SSL 库初始化 */
+		
+		OpenSSL_add_all_algorithms();/* 载入所有 SSL 算法 */
+		
+		SSL_load_error_strings();/* 载入所有 SSL 错误消息 */
+		
+		ctx = SSL_CTX_new(SSLv23_server_method());/* 以 SSL V2 和 V3 标准兼容方式产生一个 SSL_CTX ，即 SSL Content Text */
+		
+		if (ctx == NULL) {
+		ERR_print_errors_fp(stdout);
+		exit(1);
+		}/* 也可以用 SSLv2_server_method() 或 SSLv3_server_method() 单独表示 V2 或 V3标准 */
+		
+		
+		if (SSL_CTX_use_certificate_file(ctx, "cacert.pem", SSL_FILETYPE_PEM) <= 0) {
+		ERR_print_errors_fp(stdout);
+		exit(1);
+		}/* 载入用户的数字证书， 此证书用来发送给客户端。 证书里包含有公钥 */
+		
+		if (SSL_CTX_use_PrivateKey_file(ctx, "privkey.pem", SSL_FILETYPE_PEM) <= 0) {
+		ERR_print_errors_fp(stdout);
+		exit(1);
+		}/* 载入用户私钥 */
+		
+		if (!SSL_CTX_check_private_key(ctx)) {
+		ERR_print_errors_fp(stdout);
+		exit(1);
+		}/* 检查用户私钥是否正确 */
+		
+		
+		
         int sd = socket(PF_INET, SOCK_STREAM, 0);  
         if(sd == -1) oops("socket");  
   
@@ -31,15 +67,28 @@ int main(){
         int listen_return = listen(sd, 1);  
         if(listen_return == -1) oops("listen");  
   
-        while(1){  
+        while(1){
+				SSL *ssl;
                 int client_fd = accept(sd, NULL, NULL);  
-                if(client_fd == -1) oops("accept");  
-  
+                if(client_fd == -1) oops("accept!!!");  
+				
+				/* 基于 ctx 产生一个新的 SSL */
+				ssl = SSL_new(ctx);
+				/* 将连接用户的 socket 加入到 SSL */
+				SSL_set_fd(ssl, client_fd);
+				/* 建立 SSL 连接 */
+				if (SSL_accept(ssl) == -1) {
+				perror("accept@@@");
+				close(client_fd);
+				break;
+				}
+				
+				
                 time_t current_time = time(NULL); 
 				
                 char fileName[100]="";  
 				char filePath[100]="";
-				recv(client_fd, fileName, sizeof(fileName), 0);
+				SSL_read(ssl, fileName, sizeof(fileName));
 				printf("file name:%s",fileName);
                 sprintf(filePath, "%s", fileName);  
                 int fd = open(filePath, O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);  
@@ -47,7 +96,7 @@ int main(){
   
                 char buff[1024];  
                 ssize_t length;  
-                while((length = recv(client_fd, buff, sizeof(buff), 0)) > 0){  
+                while((length = SSL_read(ssl, buff, sizeof(buff))) > 0){  
                         write(fd, buff, length);  
                 }  
   
